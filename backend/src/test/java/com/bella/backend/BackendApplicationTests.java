ffbef75b5c1159c6830845782ad7475257f1eed9
Adeniyi-Bella <adeniyibella@gmail.com>
2023-08-12 09:52:45 +0200

    added boiler template from spring.io for the api

---
+++ backend/src/test/java/com/bella/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bella.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
