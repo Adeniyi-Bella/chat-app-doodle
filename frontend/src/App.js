742575f6847a0f7daf7b8d7b1608498ae5ff63c8
Adeniyi-Bella <adeniyibella@gmail.com>
2023-08-13 01:02:00 +0200

    implemented suggested UI wireframe with fake data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,117 @@
-// app.js
+import React, { useState } from "react";
 
-import React, { useEffect, useState } from 'react';
-import { getAllUsers } from './api/backendServices'; // Update the path if needed
+// Sample user data
+const users = [
+  { name: "John", message: "Brilliant", date: "2020-01-01" },
+  { name: "ninja", message: "Great resource", date: "2020-01-02" },
+  {
+    name: "ninja",
+    message:
+      "Great resource like I have alwasy said all the time and I am tired",
+    date: "2020-01-02",
+  },
+];
 
 function App() {
-  const [users, setUsers] = useState([]);
+  // State for form data to send message
+  const [formData, setFormData] = useState({
+    message: "",
+  });
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const usersData = await getAllUsers();
-        setUsers(usersData);
-      } catch (error) {
-        // Handle error if needed
-      }
+  // State for user authentication
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [signInData, setSignInData] = useState({
+    user: "",
+  });
+
+  // Handle input change in the message form when message is being typed
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  // Handle input change in the authentication form
+  const handleSignin = (event) => {
+    setSignInData((prevData) => ({ ...prevData, user: event.target.value }));
+  };
+
+  // Handle submission of the message form
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Form data submitted:", formData);
+  };
+
+  // Handle submission of the authentication form
+  const handleAuth = (event) => {
+    event.preventDefault();
+    const signedInUser = signInData.user;
+
+    // Check if the signed-in user's name matches any user in the array
+    const matchedUser = users.find((user) => user.name === signedInUser);
+
+    // Change user's name to "You" if matched
+    if (matchedUser) {
+      matchedUser.name = "You";
     }
 
-    fetchData();
-  }, []);
+    setIsAuthenticated(true); // Set authentication state to true
+  };
 
+  // Render the component
   return (
-    <div>
-      <h1>Users</h1>
-      <ul>
-        {users.map(user => (
-          <li key={user.id}>
-            {user.name} - {user.messages.map(msg => msg.message).join(', ')}
-          </li>
-        ))}
-      </ul>
+    <div className="main">
+      <div className="questionTag">
+        {!isAuthenticated ? ( // Conditional rendering based on isAuthenticated
+          <>
+            Who are You?{" "}
+            <form onSubmit={handleAuth} className="questionTag">
+              <div>
+                <input
+                  id="user"
+                  name="user"
+                  value={signInData.user}
+                  onChange={handleSignin}
+                />
+              </div>
+              <button className="sign-in" type="submit">
+                Sign in
+              </button>
+            </form>
+          </>
+        ) : (
+          <div className="border">
+            <div className="user-div">
+              {/* Map over users and conditionally add a border for matching username */}
+              {users.map((user, index) => (
+                <div
+                  key={index}
+                  className={`user-details ${
+                    user.name === "You" ? "highlighted" : ""
+                  }`}
+                >
+                  <div>{user.name}</div>
+                  <div className="message">{user.message}</div>
+                  <div>{user.date}</div>
+                </div>
+              ))}
+            </div>
+            <div>
+              {/* Main form */}
+              <form onSubmit={handleSubmit} className="form">
+                <div>
+                  <textarea
+                    id="message"
+                    name="message"
+                    value={formData.message}
+                    onChange={handleChange}
+                  />
+                </div>
+                <button type="submit">Send</button>
+              </form>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
